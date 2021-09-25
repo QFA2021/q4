c12dec20f2c5a1d9e82d66e9e9ed31c2aea39322
Magnus Kühn <magnus.khn@gmail.com>
2021-09-25 18:59:17 +0200

    Add winner detection function

--- src/ClassicGame.ts
+++ src/ClassicGame.ts
@@ -1,4 +1,4 @@
-import { World, WorldRow, Piece } from "./GameState";
+import { World, WorldRow, Piece, GameState } from "./GameState";
 
 /**
  * Inserts $piece into $world at column $column
@@ -24,4 +24,69 @@ export function insertPiece(height: number, world: World<Piece>, column: number,
     world[column] = {}
     world[column][height] = piece
     return true
+}
+
+const WIN_LENGTH = 4;
+export function computeWinner(height: number, width: number, world: World<Piece>) {
+    // columns
+    for (let column = 1; column <= width; column++) {
+        const winner = checkWinnerDirection(height, width, world,
+            1, column, 0, 1);
+        if (winner !== undefined) {
+            return winner;
+        }
+
+        // diagonals: sufficient to check diagonals going right
+        for (let row = 1; row <= height - WIN_LENGTH; row++) {
+            // check diagonal going to top right
+            const winnerTop = checkWinnerDirection(height, width, world,
+                row, column, 1, -1);
+            if (winnerTop !== undefined) {
+                return winnerTop;
+            }
+
+            // check diagonal going to bottom right
+            const winnerBottom = checkWinnerDirection(height, width, world,
+                row, column, 1, 1);
+            if (winnerBottom !== undefined) {
+                return winnerBottom;
+            }
+        }
+    }
+
+    // rows
+    for (let row = 1; row <= height; row++) {
+        const winner = checkWinnerDirection(height, width, world,
+            row, 1, 1, 0);
+        if (winner !== undefined) {
+            return winner;
+        }
+    }
+}
+
+/**
+ * Checks for a winner in direction (rowD, columnD) starting from (row, column)
+ * Return player1 as bool or undefined
+ */
+function checkWinnerDirection(height: number, width: number,
+    world: World<Piece>, row: number, column: number,
+    rowD: number, columnD: number): boolean | undefined {
+    let count = 0
+    let player1 = undefined
+
+    while (row <= height && column <= width) {
+        const nextPlayer1 = world[row]?.[column]?.player1
+        if (nextPlayer1 != player1) {
+            count = 0
+            player1 = nextPlayer1
+        } else if (++count >= WIN_LENGTH) {
+            return player1
+        }
+
+        // step into direction (rowD, columnD)
+        row += rowD
+        column += columnD
+    }
+
+    return undefined
 }
