d7b6e56edbb072f6c66a0ed6d7cc820d74e1653a
Magnus Kühn <magnus.khn@gmail.com>
2021-09-28 18:34:14 +0200

    Add service worker

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,11 @@ module.exports = {
     port: 8080,
     public: 'localhost:8080',
   },
+  pwa: {
+    name: 'q4',
+    themeColor: '#DAD6F1',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 };
